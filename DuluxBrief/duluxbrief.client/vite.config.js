899b0f813ca325691131d0476054183169ef84dc
nishanth-km <nishanth1991@gmail.com>
2025-06-16 08:43:52 +1000

    remove default weather app details

--- DuluxBrief/duluxbrief.client/vite.config.js
+++ DuluxBrief/duluxbrief.client/vite.config.js
@@ -47,7 +47,7 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '^/weatherforecast': {
+            '^/student': {
                 target,
                 secure: false
             }
